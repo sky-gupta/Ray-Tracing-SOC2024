700cbfa9d230dcedb3651a3416571181df8a5d72
Aakash Gupta <aksgupta2131@gmail.com>
2024-06-05 23:32:07 +0530

    first commit

---
+++ first_image.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int image_width = 256;
+    int image_height = 256;
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j = 0; j < image_height; j++) {
+    	clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
+        for (int i = 0; i < image_width; i++) {
+            auto b = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto r = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
---
+++ modified_image.cpp
@@ -0,0 +1,16 @@
+#include "color.h"
+#include "vec3.h"
+
+#include<iostream>
+using namespace std;
+int main(){
+    int image_width = 256;
+    int image_height = 256;
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+           auto pixel_color= color(double(i)/(image_width-1),double(j)/(image_height-1),0);
+           write_color(cout,pixel_color);
+        }
+    }
+}
