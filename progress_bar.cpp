559491cfb032254be71017a06af4a6a455050ba4
sky-gupta <aksgupta2131@gmail.com>
2024-06-08 21:13:55 +0530

    Progress bar added

--- progress_bar.cpp
+++ progress_bar.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     int image_width = 256;
     int image_height = 256;
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for (int j = 0; j < image_height; j++) {
     	clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
         for (int i = 0; i < image_width; i++) {
