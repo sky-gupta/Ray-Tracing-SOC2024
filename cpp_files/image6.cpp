891c99adfefe385c2f44984610cbcc0ad2bb28c9
sky-gupta <aksgupta2131@gmail.com>
2024-06-27 00:42:48 +0530

    Image 6 added

---
+++ cpp_files/image6.cpp
@@ -0,0 +1,19 @@
+#include<common_header.h>
+#include<hittable.h>
+#include<hittable_list.h>
+#include<sphere.h>
+#include<camera.h>
+
+int main(){
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    camera cam;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+
+    cam.render(world);
+}
--- header/color.h
+++ header/color.h
@@ -1,6 +1,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 #include "vec3.h"
+#include<interval.h>
 #include<iostream>
 using namespace std;
 using color=vec3;
@@ -10,9 +11,11 @@ void write_color(ostream& out, const color& pixel_color) {
     auto g=pixel_color.y();
     auto b=pixel_color.z();
 
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
+
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
