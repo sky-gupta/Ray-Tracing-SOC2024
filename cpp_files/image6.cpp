8b2e73b3ee9ac0d365fc3aa3fe22f3989edf1d2c
sky-gupta <aksgupta2131@gmail.com>
2024-06-27 22:45:43 +0530

    updates

--- cpp_files/image6.cpp
+++ cpp_files/image6.cpp
@@ -9,7 +9,7 @@ int main(){
 
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
-
+ 
     camera cam;
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 400;
